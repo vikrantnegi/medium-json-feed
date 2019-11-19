0359113d0ceff6be1ef60ef4519d4da7b9040b05
vikrantnegi <vikrantnegi007@gmail.com>
2019-11-19 11:19:09 +0530

    update endpoint

--- server.js
+++ server.js
@@ -4,22 +4,23 @@ const mediumJSONFeed = require('./index.js');
 const port = process.env.PORT || 3000;
 const headers = {
   'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': process.env.ORIGIN || '*'
+  'Access-Control-Allow-Origin': process.env.ORIGIN || '*',
 };
 
-http.createServer((req, res) => {
-  if (req.method !== 'GET' || ['/robots.txt', '/favicon.ico'].indexOf(req.url) !== -1) {
-    res.writeHead(204);
-    return res.end();
-  }
+http
+  .createServer((req, res) => {
+    if (req.method !== 'GET' || ['/robots.txt', '/favicon.ico'].indexOf(req.url) !== -1) {
+      res.writeHead(204);
+      return res.end();
+    }
 
-  console.log(`> GET: '${req.url}' --- ${new Date()}`);
+    console.log(`> GET: '${req.url}' --- ${new Date()}`);
 
-  mediumJSONFeed(req.url, data => {
-    res.writeHead(data.status || 500, headers);
-    res.end(JSON.stringify(data, null, 2), 'utf-8');
-  });
-
-}).listen(port);
+    mediumJSONFeed(req.url, data => {
+      res.writeHead(data.status || 500, headers);
+      res.end(JSON.stringify(data, null, 2), 'utf-8');
+    });
+  })
+  .listen(port);
 
 console.info('>>> Server listening on port', port, '\n');
